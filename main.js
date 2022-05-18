e5d0f15830ddeb5520f75c096931b1231390b08b
Golam Rabbi <spirabbi@gmail.com>
2022-05-18 23:47:13 +0600

    upload update

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const app = Vue.createApp({
-  data: function () {
+  data() {
     return {
       variants: [
-        { id: 1, name: "Vanilla", price: 150 },
-        { id: 2, name: "Chocolate", price: 100 },
-        { id: 3, name: "Orange", price: 70 },
-        { id: 4, name: "Lemon", price: 30 },
+        { id: 1, name: "Vanilla", price: 100 },
+        { id: 2, name: "Chocolate", price: 120 },
+        { id: 3, name: "Strawberry", price: 90 },
+        { id: 4, name: "Orange", price: 70 },
+        { id: 5, name: "Lemon", price: 30 },
       ],
       cart: [],
+      showModal: false,
     };
   },
   methods: {
@@ -15,11 +17,17 @@ const app = Vue.createApp({
       this.cart.push(this.variants.find((variant) => variant.id === variantId));
     },
     removeFromCart(variantId) {
-      let postion = this.cart.findIndex((variant) => variant.id === variantId);
-      this.cart.splice(postion, 1);
+      let position = this.cart.findIndex((variant) => variant.id === variantId);
+
+      this.cart.splice(position, 1);
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    clearCart() {
+      this.cart = [];
     },
   },
-
   computed: {
     total() {
       return this.cart.reduce((t, variant) => t + variant.price, 0);
